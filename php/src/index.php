4bcc4a040886b6852a4812caeb1adbbe2e1d2a9b
Stephen Parente <stephen@ingenuitydesign.com>
2014-10-01 10:25:06 -0400

    Updated to use Illustrator file names and convert based on new conventions

--- php/src/index.php
+++ php/src/index.php
@@ -10,12 +10,12 @@ function print_verbose( $string ) {
 
 function print_memory( ) {
 	global $args;
-	if ($args->showMemory()) printf("%.0dMB currently in use memory out of %.0dMB (File #%d)\n", memory_get_usage()/1024, ini_get('memory_limit'), $i);	
+	if ($args->showMemory()) printf("%.0dMB currently in use memory out of %.0dMB (File #%d)\n", memory_get_usage()/1024, ini_get('memory_limit'), $i);
 }
 
 function status_bar( $i, $news ) {
 	global $args;
-	if ($args->showProgress()) show_status($i, count($news));	
+	if ($args->showProgress()) show_status($i, count($news));
 }
 
 global $args;
@@ -27,75 +27,115 @@ if ($args->showErrors()) {
 }
 
 class IngenuityWallpaperFile {
-	
+
 	private static $wallpapers = array();
-	
+
 	private $path;
-	
+
 	private $height,$width,$month,$year,$type,$subtype;
-	
+
+	private static $conversion = array(
+
+		//this filters the wallpapers to look for certain text and merely replace it
+		'-01' => array("texture", "simple"),
+		'-02' => array("texture", "festivecal"),
+		'-03' => array("texture", "cal"),
+		'-04' => array("color1", "simple"),
+		'-05' => array("color1", "festivecal"),
+		'-06' => array("color1", "cal"),
+		'-07' => array("color2", "simple"),
+		'-08' => array("color2", "festivecal"),
+		'-09' => array("color2", "cal"),
+		'-10' => array("image", "simple"),
+		'-11' => array("image", "festivecal"),
+		'-12' => array("image", "cal"),
+		'-13' => array("texture", "quote1"),
+		'-14' => array("color1", "quote1"),
+		'-15' => array("color2", "quote1"),
+		'-16' => array("image", "quote1"),
+		'-17' => array("texture", "quote2"),
+		'-18' => array("color1", "quote2"),
+		'-19' => array("color2", "quote2"),
+		'-20' => array("image", "quote2")
+
+	);
+
 	function __construct( $filepath ) {
-		
+
 		$this->path = $filepath;
 		//now get the good stuff from the filepath
-		if (preg_match("#(?P<month>[a-z]+)(?P<year>[0-9]{2,4})_(?P<type>[^_]+)_(?P<subtype>[^_]+)_(?P<width>[0-9]{3,4})x(?P<height>[0-9]{3,4})#i", $filepath, $matches)) {
+		//two ways we can do this. Renamed our not renamed
+
+		if (preg_match("#(?P<month>[a-z]+)(?P<year>[0-9]{2,4})_(?P<width>[0-9]{3,4})x(?P<height>[0-9]{3,4})(?P<suffix>-[0-9]{2})#i", $filepath, $matches)) {
+
+			$this->month = $matches['month'];
+			$this->year = $matches['year'];
+
+			if (array_key_exists($matches['suffix'], self::$conversion))
+				list($this->type, $this->subtype) = self::$conversion[$matches['suffix']];
+			else
+				list($this->type, $this->subtype) = array('unknown', 'unknown');
+
+			$this->width = $matches['width'];
+			$this->height = $matches['height'];
+
+		} elseif (preg_match("#(?P<month>[a-z]+)(?P<year>[0-9]{2,4})_(?P<type>[^_]+)_(?P<subtype>[^_]+)_(?P<width>[0-9]{3,4})x(?P<height>[0-9]{3,4})#i", $filepath, $matches)) {
 			$this->month = $matches['month'];
 			$this->year = $matches['year'];
 			$this->type = $matches['type'];
 			$this->subtype = $matches['subtype'];
 			$this->width = $matches['width'];
 			$this->height = $matches['height'];
-			
-			//add it to the configuration file 
-			$key = sprintf('%s%s_%s.jpg', $matches['month'], $matches['year'], $matches['type']);
-			if (!array_key_exists( $key, self::$wallpapers )) {
-				
-				switch (strtolower($this->type)) {
-					case 'texture': $name = "Texture"; break;
-					case 'image': $name = "Picture This"; break;
-					case 'purple': $name = "Chill Out"; break;
-					default: $name = "Amp it Up";
-						
-				}
-				
-				self::$wallpapers[$key] = array('supports' => array('festive','cal','quote1','quote2'), 'sizes' => array(), 'guid' => $key, 'name' => $name);
-				
-			}
-			
-			//add supporting size if it doesnt already exist
-			$sizestr = sprintf("%sx%s", $this->width, $this->height );
-			if (!in_array( $sizestr, self::$wallpapers[$key]['sizes'] )) self::$wallpapers[$key]['sizes'][] = sprintf("%sx%s", $this->width, $this->height );
 
-			
 		} else {
-			print_verbose("Could not parse " . $filepath . PHP_EOL);
+			return print_verbose("Could not parse " . $filepath . PHP_EOL);
 		}
-		
-		
+
+		//add it to the configuration file
+		$key = sprintf('%s%s_%s.jpg', $this->month, $this->year, $this->type);
+		if (!array_key_exists( $key, self::$wallpapers )) {
+
+			switch (strtolower($this->type)) {
+				case 'texture': $name = "Texture"; break;
+				case 'image': $name = "Picture This"; break;
+				case 'purple': $name = "Chill Out"; break;
+				default: $name = "Amp it Up";
+
+			}
+
+			self::$wallpapers[$key] = array('supports' => array('festive','cal','quote1','quote2'), 'sizes' => array(), 'guid' => $key, 'name' => $name);
+
+		}
+
+		//add supporting size if it doesnt already exist
+		$sizestr = sprintf("%sx%s", $this->width, $this->height );
+		if (!in_array( $sizestr, self::$wallpapers[$key]['sizes'] )) self::$wallpapers[$key]['sizes'][] = sprintf("%sx%s", $this->width, $this->height );
+
+
 	}
-	
+
 	public function getHeight() {return $this->height;}
 	public function getYear() {return $this->year;}
 	public function getType() {return $this->type;}
 	public function getSubtype() {return $this->subtype;}
 	public function getWidth() {return $this->width;}
-	
+
 	private $fileWidth, $fileHeight, $mime;
-	
+
 	private $newImage;
-	
+
 	private function getFInfo() {
 		list($this->fileWidth, $this->fileHeight, $this->mime) = getimagesize($this->path);
 	}
-	
+
 	public function moveToExportFolder() {
-	
+
 	}
-	
+
 	public static function generateConfig() {
 		$wallpapers = array();
 		foreach( self::$wallpapers as $w ) {
-			$wallpapers[] = (object) $w;	
+			$wallpapers[] = (object) $w;
 		}
 		$config = array(
 			'wallpapers' => $wallpapers,
@@ -122,11 +162,15 @@ class IngenuityWallpaperFile {
 					)
 			),
 		);
-		
+
 		return $config;
-			
+
 	}
-	
+
+	private function generateFilename() {
+		return sprintf("%s%d_%s_%s_%dx%d.jpg", $this->month, $this->year, $this->type, $this->subtype, $this->width, $this->height);
+	}
+
 	public function createNew() {
 		switch ($this->mime) {
 			case IMAGETYPE_GIF:
@@ -144,14 +188,13 @@ class IngenuityWallpaperFile {
 		if ($newImg === false) {
 			return false;
 		}
-		
+
 		//get proper aspect ratio
 		$aspect = $this->width / $this->height;
 		$thumbnail_gd_image = imagecreatetruecolor($this->width, $this->height);
 		imagecopyresampled($thumbnail_gd_image, $newImg, 0, 0, 0, 0, $this->width, $this->height, $this->fileWidth, $this->fileHeight);
-		imagejpeg($thumbnail_gd_image, ABSPATH . ".tmp" . DIRECTORY_SEPARATOR . basename($this->path), 90);
-		
-		
+		imagejpeg($thumbnail_gd_image, ABSPATH . ".tmp" . DIRECTORY_SEPARATOR . $this->generateFilename(), 90);
+
 		if ($this->width == 1920) {
 			//create regular preview
 			$fileName = ABSPATH . ".tmp" . DIRECTORY_SEPARATOR . sprintf("preview_%s%d_%s_%s.jpg", $this->month, $this->year, $this->type, $this->subtype);
@@ -161,14 +204,14 @@ class IngenuityWallpaperFile {
 			$fileName = sprintf("preview_mobile_%s%d_%s_%s.jpg", $this->month, $this->year, $this->type, $this->subtype);
 			copy( $this->path, ABSPATH . '.tmp' . DIRECTORY_SEPARATOR . $fileName ); //done
 		}
-		
+
 		$newImg = NULL;
 		$thumbnail_gd_image = NULL;
-		
+
 		unset ($newImg, $thumbnail_gd_image);
-		
+
 	}
-	
+
 	public function maybeSize() {
 		global $args;
 		@$this->getFInfo();
@@ -176,9 +219,9 @@ class IngenuityWallpaperFile {
 			if ($args->isVerbose()) printf("Resizing %s from %dx%d to %dx%d\n", basename($this->path), $this->fileWidth, $this->fileHeight, $this->width, $this->height);
 			$this->createNew();
 		}
-		
+
 	}
-	
+
 }
 
 function ListIn($dir, $prefix = '') {
@@ -226,18 +269,22 @@ if (!$args->noResize()) {
 	}
 	print_verbose($i . " files resized and put in the .tmp folder\n");
 	print_verbose("Generating configuration file.\n");
-	
+
 	$conf = IngenuityWallpaperFile::generateConfig();
 	$json = json_encode($conf);
-	
+
 	//we got the script now we can write it
 	print_verbose("Writing configuration file.\n");
     Phar::interceptFileFuncs();
-    if (file_exists( ABSPATH . '.tmp/conf.php')) {
-        $handle = fopen( '.tmp/conf.php', 'w+' );
-        fwrite( $handle, $json );
-        fclose( $handle );
-    } else {
+	try {
+        $handle = fopen( ABSPATH . '.tmp ' . DIRECTORY_SEPARATOR . 'conf.php', 'w+' );
+		if ($handle) {
+			fwrite( $handle, $json );
+			fclose( $handle );
+		} else {
+			throw new Exception("Couldn't open conf: " . $input . '/.tmp/conf.php' );
+		}
+    } Catch (Exception $e) {
         echo "We were not able to find a conf.php file" . PHP_EOL;
     }
 
@@ -246,19 +293,19 @@ if (!$args->noResize()) {
 if ($args->FTP()):
 
 	echo "Now to try to upload them via FTP!\n";
-	
+
 	$conf = parse_ini_file('conf.ini');
 	$ftp_server = isset($conf['server']) ? $conf['server'] : false;
 	$ftp_user_name= isset($conf['user']) ? $conf['user'] : false;
 	$ftp_user_pass= isset($conf['password']) ? $conf['password'] : false;
 	$remote_file = isset($conf['path']) ? $conf['path'] : "/%s";
-	// set up basic connection 
-	$conn_id = ftp_connect($ftp_server); 
-	
-	// login with username and password 
-	$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
+	// set up basic connection
+	$conn_id = ftp_connect($ftp_server);
+
+	// login with username and password
+	$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 	ftp_pasv( $conn_id, true );
-	// upload a file 
+	// upload a file
 	$news = ListIn( ABSPATH . '.tmp');
 	try {
 		$i = 0;
@@ -267,34 +314,34 @@ if ($args->FTP()):
 			$i++;
 			status_bar( $i, $news);
 			if (basename($file) == "conf.php") {
-				//something special happens here	
-				if (ftp_put($conn_id, '/wallpaper/ajax/conf.php', $file, FTP_ASCII)) { 
-					print_verbose( "Successfully updated configuration\n" ); 
-				} else { 
+				//something special happens here
+				if (ftp_put($conn_id, '/wallpaper/ajax/conf.php', $file, FTP_ASCII)) {
+					print_verbose( "Successfully updated configuration\n" );
+				} else {
 					print_verbose( "There was a problem while uploading configuration\n" );
-				} 		
+				}
 			} else {
-				if (ftp_put($conn_id, sprintf($remote_file, basename($file)), $file, FTP_BINARY )) { 
-					print_verbose( "Successfully uploaded $file\n" ); 
-				} else { 
-					print_verbose( "There was a problem while uploading $file\n" ); 
-				} 
+				if (ftp_put($conn_id, sprintf($remote_file, basename($file)), $file, FTP_BINARY )) {
+					print_verbose( "Successfully uploaded $file\n" );
+				} else {
+					print_verbose( "There was a problem while uploading $file\n" );
+				}
 			}
 		}
-	
+
 	} catch (Exception $e) {
 		echo "Terminated\n";
 	}
-	ftp_close($conn_id); 
+	ftp_close($conn_id);
 	print_verbose( "Closed connection\n" );
-	
+
 endif;
 
 echo "Cleaning .tmp directory\n";
 
 foreach(ListIn('.tmp') as $file ) {
 	$file = ABSPATH . '.tmp' . DIRECTORY_SEPARATOR . $file;
-	@unlink($file);	
+	@unlink($file);
 }
 
 
